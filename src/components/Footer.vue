0219f625deb9cb5311c1474cdf8a9c80d68d1d67
MitJanta <godlovemit777@gmail.com>
2021-10-23 19:55:57 +0700

    Footer Dialog add

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,6 +7,39 @@
         src="../assets/images/peep-sit-1man-left.png"
         alt="not found image"
       />
+
+      <div
+        class="pray-dialog"
+        v-if="currentRouteName == 'HomePage' ? true : false"
+      >
+        <p>
+          “อยากให้อธิษฐานเผื่อ เรื่องอะไรเล่าได้เลยนะ”
+          <small>พวกเราพร้อมจะรับฟังและอธิษฐานเผื่อ</small>
+        </p>
+
+        <button class="btn btn-custom-yellow2 m-3 text-white">
+          <span style="font-size: 1.5em; margin-top: -0.2em; position: absolute"
+            >+</span
+          >
+          &nbsp;&nbsp;&nbsp;เริ่มเขียนที่นี่
+        </button>
+      </div>
+
+      <div
+        class="q-a-dialog"
+        v-if="currentRouteName == 'HomePage' ? true : false"
+      >
+        <p>
+          ฉันก็มีเรื่องที่สงสัย???<br>
+          <small>สร้างกระดานคำถามเลย</small>
+        </p>
+        <button class="btn btn-custom-primary mx-3 mb-3 text-white">
+      <span style="font-size: 1.5em; margin-top: -0.2em; position: absolute"
+        >+</span
+      >
+      &nbsp;&nbsp;&nbsp;สร้างคำถามใหม่
+    </button>
+      </div>
     </div>
 
     <div class="footer-top-extension">
@@ -23,6 +56,7 @@
         />
       </div>
     </div>
+
     <div class="footer-content">
       <div class="container">
         <div class="row float-right">
@@ -33,7 +67,9 @@
             alt="logo"
           />
           <p>
-            <span class="font-weight-bold text-primary ml-n4">Christ Space</span>
+            <span class="font-weight-bold text-primary ml-n4"
+              >Christ Space</span
+            >
             พื้นที่ปลอดภัยสำหรับคริสเตียน
           </p>
         </div>
@@ -62,7 +98,7 @@ export default {
   width: 100%;
   position: relative;
   bottom: 0px;
-  margin-top: auto;
+  margin-top: 80px;
   margin-bottom: 0px;
 }
 
@@ -104,5 +140,90 @@ export default {
   right: 150px;
   width: 200px;
   bottom: 0px;
+}
+
+.pray-dialog {
+  position: absolute;
+  top: -60px;
+  left: 220px;
+  padding: 3em 1em;
+  border-radius: 0.7em;
+  background-color: #fff3f8;
+  max-width: 300px;
+  border: #18191f solid 2px;
+  box-shadow: 0px 4px 0px black;
+  z-index: 999;
+}
+
+.pray-dialog p {
+  text-align: center;
+  color: black;
+  font-size: 1.3em;
+  font-weight: 700;
+}
+
+.pray-dialog small {
+  margin-top: -10px;
+  margin-left: 5px;
+  color: black;
+  font-size: 0.6em;
+  font-weight: 400;
+}
+
+.q-a-dialog {
+  position: absolute;
+  top: -30px;
+  right: 150px;
+  padding: 1em;
+  border-radius: 0.7em;
+  background-color: #FFBE66;
+  max-width: 500px;
+  width: 400px;
+  border: #18191f solid 2px;
+  box-shadow: 0px 4px 0px black;
+  z-index: 999;
+}
+
+.q-a-dialog p {
+  text-align: left;
+  color: black;
+  font-size: 1.3em;
+  font-weight: 700;
+}
+
+.q-a-dialog small {
+  margin-top: -10px;
+  margin-left: 5px;
+  color: black;
+  font-size: 0.6em;
+  font-weight: 400;
+}
+
+.btn-custom-yellow2 {
+  background-color: #f9a93e;
+  font-size: 1.1em;
+  border-radius: 0.6em;
+  color: black !important;
+  transition: 0.3s;
+}
+
+.btn-custom-yellow2:hover {
+  box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);
+  color: black !important;
+  transform: translate(-1px, -1px);
+}
+
+.btn-custom-primary {
+  background-color: #397490;
+  font-size: 1.1em;
+  border: #18191f solid 2px;
+  border-radius: 0.6em;
+  color: white !important;
+  transition: 0.3s;
+}
+
+.btn-custom-primary:hover {
+  box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);
+  transform: translate(-1px, -1px);
 }
 </style>
--- src/components/Modals_Popovers/PopoverCreate.vue
+++ src/components/Modals_Popovers/PopoverCreate.vue
@@ -1,45 +1,50 @@
 <template>
-    <b-popover target="popover-create" title="" placement="bottom" class="p-4">
-      <button class="btn btn-custom-yellow2 m-3 text-white">
-              <span style="font-size:1.5em; margin-top:-.2em; position: absolute">+</span> &nbsp;&nbsp;&nbsp;ต้องการคำอธิษฐานเผื่อ
-       </button>
-       <button class="btn btn-custom-primary mx-3 mb-3 text-white">
-              <span style="font-size:1.5em; margin-top:-.2em; position: absolute">+</span> &nbsp;&nbsp;&nbsp;สร้างคำถามใหม่
-       </button>
-    </b-popover>
+  <b-popover target="popover-create" title="" placement="bottom" class="p-4">
+    <button class="btn btn-custom-yellow2 m-3 text-white">
+      <span style="font-size: 1.5em; margin-top: -0.2em; position: absolute"
+        >+</span
+      >
+      &nbsp;&nbsp;&nbsp;ต้องการคำอธิษฐานเผื่อ
+    </button>
+    <button class="btn btn-custom-primary mx-3 mb-3 text-white">
+      <span style="font-size: 1.5em; margin-top: -0.2em; position: absolute"
+        >+</span
+      >
+      &nbsp;&nbsp;&nbsp;สร้างคำถามใหม่
+    </button>
+  </b-popover>
 </template>
 
 <script>
-
 </script>
 
 <style lang="scss" scope>
 .btn-custom-yellow2 {
-    width: 215px;
-    background-color: #f9a93e;
-    border: #18191f solid 2px;
-    border-radius: 0.6em;
-    color:black !important;
-    transition: .3s;
-  }
-  
-  .btn-custom-yellow2:hover {
-    box-shadow: 2px 2px 2px rgba(0,0,0,.5);
-    color: black !important;
-    transform: translate(-1px,-1px);
-  }
+  width: 215px;
+  background-color: #f9a93e;
+  border: #18191f solid 2px;
+  border-radius: 0.6em;
+  color: black !important;
+  transition: 0.3s;
+}
 
-  .btn-custom-primary {
-    width: 215px;
-    background-color: #397490;
-    border: #18191f solid 2px;
-    border-radius: 0.6em;
-    color:white !important;
-    transition: .3s;
-  }
+.btn-custom-yellow2:hover {
+  box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);
+  color: black !important;
+  transform: translate(-1px, -1px);
+}
 
-  .btn-custom-primary:hover {
-    box-shadow: 2px 2px 2px rgba(0,0,0,.5);
-    transform: translate(-1px,-1px);
-  }
+.btn-custom-primary {
+  width: 215px;
+  background-color: #397490;
+  border: #18191f solid 2px;
+  border-radius: 0.6em;
+  color: white !important;
+  transition: 0.3s;
+}
+
+.btn-custom-primary:hover {
+  box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);
+  transform: translate(-1px, -1px);
+}
 </style>
