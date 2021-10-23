d86f9d1907164fc4e76975bb7aab62fab910fe65
MitJanta <godlovemit777@gmail.com>
2021-10-23 10:31:01 +0700

    ขอย้าย Header กับ Footer css ต่างๆแยกออกมาจาก Global.scss เลยนะครับ

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "./styles/global.scss";
+import "./styles/header.scss";
+import "./styles/footer.scss";
 
 Vue.config.productionTip = false;
 
